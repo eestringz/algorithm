7dc26a7135207e2d325d4893e4f3e7f6ecfd0389
eestingz <143936094+eestingz@users.noreply.github.com>
2023-09-19 17:37:46 +0900

    [Silver II] Title: N번째 큰 수, Time: 860 ms, Memory: 121308 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr += list(map(int, input().split()))
+    arr = sorted(arr, reverse=True)[:n]
+print(arr[-1])
