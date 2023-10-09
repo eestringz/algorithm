f261638104ed8087a801b1a802883e5730732826
eestingz <143936094+eestingz@users.noreply.github.com>
2023-10-10 00:18:02 +0900

    [Bronze V] Title: 시험 성적, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+num = int(input())
+
+if (90<= num <=100):
+    print('A')
+elif num//10 ==8:
+    print('B')
+elif num//10 ==7:
+    print('C')
+elif num//10 ==6:
+    print('D')
+else:
+    print('F')
