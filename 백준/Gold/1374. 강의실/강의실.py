1fa3a62dc83325875a6c1255a9fc3f7391c13128
eestingz <143936094+eestingz@users.noreply.github.com>
2023-10-11 17:03:31 +0900

    [Gold V] Title: 강의실, Time: 504 ms, Memory: 132476 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -1,20 +1,19 @@
-import heapq
+import sys, heapq
 
+input = sys.stdin.readline
 n = int(input())
-q = []
+arr = []
 for _ in range(n):
-    a, b, c = map(int, input().split())
-    q.append([b, c])
-
-q.sort(key=lambda x: (x[0], x[1]))
+    num, s, e = map(int, input().split())
+    arr.append([s, e])
+arr.sort(key=lambda x: (x[0], x[1]))
 
 heap = []
-heapq.heappush(heap, q[0][1])
-
+heapq.heappush(heap, arr[0][1])
 for i in range(1, n):
-    if q[i][0] < heap[0]:
-        heapq.heappush(heap, q[i][1])
-    else:
+    if arr[i][0] < heap[0]:  # 시작시간이 heap의 top보다 더 작으면 heap에 추가.
+        heapq.heappush(heap, arr[i][1])
+    else:  # 시작시간이 heap의 top보다 크거나 같으면, heap의 top은 pop하고 새로운 종료시간을 추가.
         heapq.heappop(heap)
-        heapq.heappush(heap, q[i][1])
+        heapq.heappush(heap, arr[i][1])
 print(len(heap))
