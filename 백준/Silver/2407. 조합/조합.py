b03053ebd9359d81d2f142999082f7f815baaf18
이승현 <143936094+eestringz@users.noreply.github.com>
2024-10-02 21:05:13 +0900

    [Silver III] Title: 조합, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+print(factorial(n) // (factorial(n - m) * factorial(m)))
