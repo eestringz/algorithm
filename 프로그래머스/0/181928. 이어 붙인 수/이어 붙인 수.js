8889cb433345e4b3351a1464b197ba815636052d
이승현 <143936094+eestringz@users.noreply.github.com>
2024-12-29 23:05:54 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    let odd = '';
+    let even = '';
+    num_list.forEach(num => num % 2 === 0 ? even += num : odd += num);
+    
+    return Number(odd) + Number(even);
+}
